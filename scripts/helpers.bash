e163670847f4e61f0b4214ea5a99c6ce0c7f9d69
Sam Salisbury <samsalisbury@gmail.com>
2022-03-10 12:50:46 +0000

    remove debug output

--- scripts/helpers.bash
+++ scripts/helpers.bash
@@ -9,7 +9,6 @@ dev_tags_validation() {
   if [ -n "${1}" ]; then
     for dt in ${1}; do
       # dev_tags can only push to the 'hashicorppreview' dockerhub org
-      echo "dt is: $dt"
       if ! grep -E "^(docker\.io/)?hashicorppreview/.*" <<<"$dt"; then
         log "dev_tags must begin with 'hashicorppreview/' or 'docker.io/hashicorppreview/' (Got: $dt)"
         return 1
