cd6bae3f33a78a0a950549440bc92f08fca859c0
Sam Salisbury <samsalisbury@gmail.com>
2022-03-25 15:52:26 +0000

    explicit return zeroes needed

--- scripts/validators.bash
+++ scripts/validators.bash
@@ -8,14 +8,14 @@ REDHAT_TAG_PREFIX="scan.connect.redhat.com/"
 REDHAT_TAG_PATTERN='^scan\.connect\.redhat\.com\/ospid\-[^[:space:]]+\/[^[:space:]]+:[^[:space:]]+$'
 
 is_not_redhat_tag() {
-	[[ $1 == $REDHAT_TAG_PREFIX* ]] || return
+	[[ $1 == $REDHAT_TAG_PREFIX* ]] || return 0
 	log "Error: found a tag beginning '$REDHAT_TAG_PREFIX' in the tags input."
 	log "A tag matching this pattern may be set in the redhat_tag input."
 	return 1
 }
 
 is_valid_redhat_tag() {
-	grep -qE "${REDHAT_TAG_PATTERN}" <<< "$1" && return
+	grep -qE "${REDHAT_TAG_PATTERN}" <<< "$1" && return 0
 	log "Error: redhat_tag must match the pattern '$REDHAT_TAG_PATTERN'"
 	log "Other tags must go in either the 'tags' or 'dev_tags' input."
 	return 1
